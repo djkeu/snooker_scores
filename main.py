858dcf9b2878eda73e5735e1f998b1bfdad22202
Marc Kooij <mrkooij@gmail.com>
2025-02-08 14:19:00 +0100

    Move ToDo's, Snooker rules to readme.md

--- main.py
+++ main.py
@@ -9,25 +9,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-# ToDo Section
-"""
-- ToDo: assign key 'p'/'f' to snookers/penalties/fouls
-- ToDo: snookers needed stage
-- ToDo: penalties for misses
-- ToDo: possibility for putting ball back after miss
-- ToDo: lose game after three misses
-"""
-
-# Snooker rules
-"""
-Points are also scored if the opponent makes the following mistakes:
-    Do not touch any ball with the cue ball,
-    Hitting or pocketing a wrong color ball,
-    Pocketing the cue ball,
-    Get a ball out of the playing area,
-    Use another cue ball than the white ball,
-    Push balls.
-"""
