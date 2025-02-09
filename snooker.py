97c17e368de0163af9780e077ed7979ff6d24f58
djkeu <mrkooij@gmail.com>
2025-02-09 23:48:09 +0100

    Add Note: to display_next_ball()

--- snooker.py
+++ snooker.py
@@ -147,6 +147,7 @@ class SnookerScores:
 
     def display_next_ball(self):
         """Display which ball the current player must pot next."""
+        # Note: prints possibly abundant, leave for now
         if self.red_needed_next:
             if self.player_1_turn:
                 print("Player 1 must pot a red ball next.")
