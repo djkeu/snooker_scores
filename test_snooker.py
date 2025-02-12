3fe21cb5461444688cdd96c288575b67cc8eb477
Marc Kooij <mrkooij@gmail.com>
2025-02-12 09:20:09 +0100

    Remove / change explanatory comments

--- test_snooker.py
+++ test_snooker.py
@@ -89,7 +89,7 @@ def test_handle_color_ball():
 def test_handle_color_ball_reduces_available():
     """Test that handle_color_ball reduces available points correctly."""
     game = SnookerScores()
-    game.red_needed_next = False  # Simulate that a red ball was just potted
+    game.red_needed_next = False  # red ball was just potted
     game.handle_color_ball(2)
     assert game.available == 140  # 147 - 7
 
@@ -120,7 +120,7 @@ def test_set_starting_scores_valid():
         assert game.score_player_1 == 20
         assert game.score_player_2 == 15
         assert game.red_balls == 10
-        assert game.available == 112  # 147 - (20 + 15)
+        assert game.available == 112
 
 
 def test_set_starting_scores_invalid():
@@ -145,9 +145,8 @@ def test_red_balls_phase():
             game.red_balls_phase()
             assert game.red_balls == 0
             assert game.score_player_1 == 48
-            # (15 * 1) + (14 * 2) + 5 (last blue ball)
             assert game.available == 29
-            # 147 - (15 * 1) - (14 * 7) - 5 (last blue ball)
+            # 147 - (15) - (98) - 5 (last blue ball)
             mocked_print.assert_any_call(
                 "\nNo more red balls left! "
                 "Pot a colored ball to start the endgame."
@@ -159,16 +158,16 @@ def test_colored_balls_phase():
     game = SnookerScores()
     game.red_balls = 0
     game.available = 27
-    game.score_player_1 = 0  # Reset player scores
-    game.score_player_2 = 0  # Reset player scores
-    game.color_needed = 2  # Start with the yellow ball
+    game.score_player_1 = 0
+    game.score_player_2 = 0
+    game.color_needed = 2
 
     with patch("builtins.input", side_effect=["2", "3", "4", "5", "6", "7"]):
         with patch("builtins.print") as mocked_print:
             game.colored_balls_phase()
             assert game.score_player_1 == 27  # Player 1 pots all colored balls
-            assert game.score_player_2 == 0   # Player 2 does not score
-            assert game.available == 0        # All points have been potted
+            assert game.score_player_2 == 0
+            assert game.available == 0
             assert game.color_needed == 8
             mocked_print.assert_any_call("\nEntering colored balls endgame!\n")
             mocked_print.assert_any_call("Available for endgame: 27")
