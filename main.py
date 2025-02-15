fbdc25fda17de8c316a5bbb551eae45909ef9910
Marc Kooij <mrkooij@gmail.com>
2025-02-15 17:24:09 +0100

    Rename instance score to scores

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from snooker import SnookerScores
 
 def main():
     """Show the snooker scores."""
-    score = SnookerScores()
-    score.start_game()
+    scores = SnookerScores()
+    scores.start_game()
 
 
 if __name__ == "__main__":
