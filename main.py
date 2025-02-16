1dbc86e161ebff9efb7bb990e3c0804545f51bee
djkeu <mrkooij@gmail.com>
2025-02-16 17:48:51 +0100

    pep8

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 from snooker_gui import SnookerGUI
 
+
 def main():
     """Start the Snooker GUI."""
     root = tk.Tk()
     app = SnookerGUI(root)
     app.start()
+
 
 if __name__ == "__main__":
     main()
