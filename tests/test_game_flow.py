375cf69dd4fb6dee580fdd1f26fc96deb5d1a1dc
djkeu <mrkooij@gmail.com>
2025-02-23 22:59:26 +0100

    Add another edge case test

--- tests/test_game_flow.py
+++ tests/test_game_flow.py
@@ -251,6 +251,19 @@ def test_multiple_penalties(capsys):
     assert "Penalty of 5 points applied to Player 1." in captured.out
     assert "Switching players..." in captured.out
 
+def test_switch_players_red_ball_phase(capsys):
+    game = SnookerScores()
+    inputs = generate_inputs(
+        [SET_SCORES_INPUT, "15", "0", "0",
+         "x",
+         QUIT_INPUT]
+    )
+    with patch("builtins.input", side_effect=inputs):
+        with pytest.raises(SystemExit):
+            game.start_game()
+    captured = capsys.readouterr()
+    assert "Switching players..." in captured.out
+    assert "Player 2 must pot a red ball next" in captured.out
 
 
 def test_game_flow():
