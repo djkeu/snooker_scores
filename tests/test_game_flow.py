7531757c12199a69bbfce5c17b9cd823ded7621c
Marc Kooij <mrkooij@gmail.com>
2025-02-25 12:01:56 +0100

    Readability teset_game_flow.py

--- tests/test_game_flow.py
+++ tests/test_game_flow.py
@@ -21,6 +21,7 @@ def generate_inputs(*sequences):
     return [str(item) for sequence in sequences for item in sequence]
 
 
+# diverse tests
 def test_game_flow():
     game = SnookerScores()
     inputs = generate_inputs([VALID_INPUTS[0], VALID_INPUTS[4], VALID_INPUTS[1], VALID_INPUTS[3], VALID_INPUTS[0], VALID_INPUTS[2], QUIT_INPUT])
@@ -28,6 +29,35 @@ def test_game_flow():
         with pytest.raises(SystemExit):
             game.red_balls_phase()
             game.colored_balls_phase()
+
+def test_multiple_penalties(capsys):
+    game = SnookerScores()
+    inputs = generate_inputs(
+        [PENALTY_INPUT, "4", "n",
+         PENALTY_INPUT, "5", "y",
+         QUIT_INPUT]
+    )
+    with patch("builtins.input", side_effect=inputs):
+        with pytest.raises(SystemExit):
+            game.start_game()
+    captured = capsys.readouterr()
+    assert "Penalty of 4 points applied to Player 1." in captured.out
+    assert "Penalty of 5 points applied to Player 1." in captured.out
+    assert "Switching players..." in captured.out
+
+def test_switch_players_red_ball_phase(capsys):
+    game = SnookerScores()
+    inputs = generate_inputs(
+        [SET_SCORES_INPUT, "15", "0", "0",
+         "x",
+         QUIT_INPUT]
+    )
+    with patch("builtins.input", side_effect=inputs):
+        with pytest.raises(SystemExit):
+            game.start_game()
+    captured = capsys.readouterr()
+    assert "Switching players..." in captured.out
+    assert "Player 2 must pot a red ball next" in captured.out
 
 
 # start_game tests
@@ -246,32 +276,3 @@ def test_start_game_multiple_invalid_inputs(capsys):
             game.start_game()
     captured = capsys.readouterr()
     assert "Only numbers between 0 and 7 are valid!" in captured.out
-
-def test_multiple_penalties(capsys):
-    game = SnookerScores()
-    inputs = generate_inputs(
-        [PENALTY_INPUT, "4", "n",
-         PENALTY_INPUT, "5", "y",
-         QUIT_INPUT]
-    )
-    with patch("builtins.input", side_effect=inputs):
-        with pytest.raises(SystemExit):
-            game.start_game()
-    captured = capsys.readouterr()
-    assert "Penalty of 4 points applied to Player 1." in captured.out
-    assert "Penalty of 5 points applied to Player 1." in captured.out
-    assert "Switching players..." in captured.out
-
-def test_switch_players_red_ball_phase(capsys):
-    game = SnookerScores()
-    inputs = generate_inputs(
-        [SET_SCORES_INPUT, "15", "0", "0",
-         "x",
-         QUIT_INPUT]
-    )
-    with patch("builtins.input", side_effect=inputs):
-        with pytest.raises(SystemExit):
-            game.start_game()
-    captured = capsys.readouterr()
-    assert "Switching players..." in captured.out
-    assert "Player 2 must pot a red ball next" in captured.out
