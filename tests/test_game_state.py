fb1dd152af3365ff724f4b5672d50f3c4a231606
Marc Kooij <mrkooij@gmail.com>
2025-04-23 15:52:25 +0200

    test*colored*player1: add arg active_player

--- tests/test_game_state.py
+++ tests/test_game_state.py
@@ -124,7 +124,7 @@ def test_display_colored_ball_to_play_player1():
     scores.player_1_turn = True
     scores.yellow_ball = 3
     
-    scores.display_colored_ball_to_play()
+    scores.display_colored_ball_to_play(active_player=scores.player_1)
 
 
 def test_display_colored_ball_to_play_player2():
